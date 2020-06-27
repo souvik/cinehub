707c3bb1edff72d1f149d85ddf888c39133cf845
Souvik Kumar Khan <souvik.khan@ymail.com>
2020-06-27 23:38:31 +0530

    Reserving seats by group

--- lib/cinehub/theatre.rb
+++ lib/cinehub/theatre.rb
@@ -1,5 +1,49 @@
 module Cinehub
+  module SeatAllocator
+    def find_preferable(seats_by_row)
+      selected = nil
+      column_numbers = seats_by_row.map(&:column)
+      mid_seat = columns / 2
+      mid_seat = (columns % 2).zero? ? mid_seat : mid_seat + 1
+      if column_numbers.include?(mid_seat)
+        selected = seats_by_row.find{ |s| s.column == mid_seat }
+        #selected.mark_reserved
+      end
+      selected
+    end
+
+    def find_and_mark_by_group(viewers, preferred_seat)
+      selected = [ preferred_seat ]
+      preferred_row = preferred_seat.row
+      preferred_col = preferred_seat.column
+      row_seats = seats[preferred_row.to_sym]
+      preferred_index = row_seats.index(preferred_seat)
+      i = 1
+      while selected.size < viewers
+        prev_index = preferred_index - i
+        if prev_index >= 0
+          prev_seat = row_seats[prev_index]
+          selected.unshift(prev_seat) if prev_seat.column == (preferred_col - i) && prev_seat.available?
+        end
+
+        break if selected.size == viewers
+
+        next_index = preferred_index + i
+        if next_index < row_seats.length
+          next_seat = row_seats[next_index]
+          selected.push(next_seat) if next_seat.column == (preferred_col + i) && next_seat.available?
+        end
+        break if prev_index < 0 && next_index > row_seats.length
+        i += 1
+      end
+      selected = [] if selected.size != viewers
+      selected
+    end
+  end
+
   class Theatre
+    include SeatAllocator
+
     attr_reader :rows, :columns, :seats
 
     def initialize(rows, columns, seats={})
@@ -10,18 +54,17 @@ module Cinehub
 
     def reserve_seats(num_of_viewer)
       raise UnavailableSeatError.new('No seat available!') if seats.empty?
-      selected = nil
+      selected = []
       seats.each_pair do |row, row_seats|
         next unless row_seats.size >= num_of_viewer
-        column_seats = row_seats.map(&:column)
-        mid_seat = columns / 2
-        mid_seat = (columns % 2).zero? ? mid_seat : mid_seat + 1
-        if column_seats.include?(mid_seat)
-          selected = row_seats.find{ |s| s.column == mid_seat }
-          selected.mark_reserved
+        seat = find_preferable(row_seats)
+        unless seat.nil?
+          selected = find_and_mark_by_group(num_of_viewer, seat)
+          next if selected.size != num_of_viewer
           break
         end
       end
+      raise UnavailableSeatError.new('No seat available!') if selected.empty?
       selected
     end
 
--- spec/cinehub/theatre_spec.rb
+++ spec/cinehub/theatre_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Cinehub::Theatre do
         a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
       })
       reserved_seat = theatre.reserve_seats(1)
-      expect(reserved_seat.id).to eq('a3')
+      expect(reserved_seat.map(&:id)).to eq(['a3'])
     end
 
     it "reserves seat b3 if a3 is unavailable" do
@@ -37,7 +37,7 @@ RSpec.describe Cinehub::Theatre do
         a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
       })
       reserved_seat = theatre.reserve_seats(1)
-      expect(reserved_seat.id).to eq('b3')
+      expect(reserved_seat.map(&:id)).to eq(['b3'])
     end
 
     it "reserves seat from second row if fist row is unavailable" do
@@ -50,7 +50,108 @@ RSpec.describe Cinehub::Theatre do
         a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
       })
       reserved_seat = theatre.reserve_seats(1)
-      expect(reserved_seat.row).to eq('b')
+      expect(reserved_seat.map(&:row)).to eq(['b'])
+    end
+
+    it "reserves seat a2 and a3" do
+      theatre = Cinehub::Theatre.new(2, 5, {
+        b5: { id: 'b5', row: 'b', column: 5, status: 'AVAILABLE' },
+        b1: { id: 'b1', row: 'b', column: 1, status: 'AVAILABLE' },
+        a2: { id: 'a2', row: 'a', column: 2, status: 'AVAILABLE' },
+        b2: { id: 'b2', row: 'b', column: 2, status: 'AVAILABLE' },
+        a3: { id: 'a3', row: 'a', column: 3, status: 'AVAILABLE' },
+        b3: { id: 'b3', row: 'b', column: 3, status: 'AVAILABLE' },
+        a4: { id: 'a4', row: 'a', column: 4, status: 'AVAILABLE' },
+        b4: { id: 'b4', row: 'b', column: 4, status: 'AVAILABLE' },
+        a5: { id: 'a5', row: 'a', column: 5, status: 'AVAILABLE' },
+        a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
+      })
+      reserved_seats = theatre.reserve_seats(2)
+      expect(reserved_seats.map(&:id)).to eq(['a2', 'a3'])
+    end
+
+    it "reserves seats a3 and a4" do
+      theatre = Cinehub::Theatre.new(2, 5, {
+        b5: { id: 'b5', row: 'b', column: 5, status: 'AVAILABLE' },
+        b1: { id: 'b1', row: 'b', column: 1, status: 'AVAILABLE' },
+        b2: { id: 'b2', row: 'b', column: 2, status: 'AVAILABLE' },
+        a3: { id: 'a3', row: 'a', column: 3, status: 'AVAILABLE' },
+        b3: { id: 'b3', row: 'b', column: 3, status: 'AVAILABLE' },
+        a4: { id: 'a4', row: 'a', column: 4, status: 'AVAILABLE' },
+        b4: { id: 'b4', row: 'b', column: 4, status: 'AVAILABLE' },
+        a5: { id: 'a5', row: 'a', column: 5, status: 'AVAILABLE' },
+        a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
+      })
+      reserved_seats = theatre.reserve_seats(2)
+      expect(reserved_seats.map(&:id)).to eq(['a3', 'a4'])
+    end
+
+    it "reserves seats b2 and b3" do
+      theatre = Cinehub::Theatre.new(2, 5, {
+        b5: { id: 'b5', row: 'b', column: 5, status: 'AVAILABLE' },
+        b1: { id: 'b1', row: 'b', column: 1, status: 'AVAILABLE' },
+        b2: { id: 'b2', row: 'b', column: 2, status: 'AVAILABLE' },
+        a3: { id: 'a3', row: 'a', column: 3, status: 'AVAILABLE' },
+        b3: { id: 'b3', row: 'b', column: 3, status: 'AVAILABLE' },
+        b4: { id: 'b4', row: 'b', column: 4, status: 'AVAILABLE' },
+        a5: { id: 'a5', row: 'a', column: 5, status: 'AVAILABLE' },
+        a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
+      })
+      reserved_seats = theatre.reserve_seats(2)
+      expect(reserved_seats.map(&:id)).to eq(['b2', 'b3'])
+    end
+
+    it "reserves seats a2, a3 and a4" do
+      theatre = Cinehub::Theatre.new(2, 5, {
+        b5: { id: 'b5', row: 'b', column: 5, status: 'AVAILABLE' },
+        b1: { id: 'b1', row: 'b', column: 1, status: 'AVAILABLE' },
+        a2: { id: 'a2', row: 'a', column: 2, status: 'AVAILABLE' },
+        b2: { id: 'b2', row: 'b', column: 2, status: 'AVAILABLE' },
+        a3: { id: 'a3', row: 'a', column: 3, status: 'AVAILABLE' },
+        b3: { id: 'b3', row: 'b', column: 3, status: 'AVAILABLE' },
+        a4: { id: 'a4', row: 'a', column: 4, status: 'AVAILABLE' },
+        b4: { id: 'b4', row: 'b', column: 4, status: 'AVAILABLE' },
+        a5: { id: 'a5', row: 'a', column: 5, status: 'AVAILABLE' },
+        a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
+      })
+      reserved_seats = theatre.reserve_seats(3)
+      expect(reserved_seats.map(&:id)).to eq(['a2', 'a3', 'a4'])
+    end
+
+    it "fail to reserve seats" do
+      theatre = Cinehub::Theatre.new(2, 5, {
+        b5: { id: 'b5', row: 'b', column: 5, status: 'AVAILABLE' },
+        b1: { id: 'b1', row: 'b', column: 1, status: 'AVAILABLE' },
+        a3: { id: 'a3', row: 'a', column: 3, status: 'AVAILABLE' },
+        b3: { id: 'b3', row: 'b', column: 3, status: 'AVAILABLE' },
+        a5: { id: 'a5', row: 'a', column: 5, status: 'AVAILABLE' },
+        a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' }
+      })
+      expect{
+        theatre.reserve_seats(3)
+      }.to raise_error(Cinehub::UnavailableSeatError, "No seat available!")
+    end
+
+    it "reserves seats a3, a4, a5 and a6" do
+      theatre = Cinehub::Theatre.new(2, 10, {
+        b5: { id: 'b5', row: 'b', column: 5, status: 'AVAILABLE' },
+        b1: { id: 'b1', row: 'b', column: 1, status: 'AVAILABLE' },
+        a2: { id: 'a2', row: 'a', column: 2, status: 'AVAILABLE' },
+        b2: { id: 'b2', row: 'b', column: 2, status: 'AVAILABLE' },
+        a3: { id: 'a3', row: 'a', column: 3, status: 'AVAILABLE' },
+        b3: { id: 'b3', row: 'b', column: 3, status: 'AVAILABLE' },
+        a4: { id: 'a4', row: 'a', column: 4, status: 'AVAILABLE' },
+        b4: { id: 'b4', row: 'b', column: 4, status: 'AVAILABLE' },
+        a5: { id: 'a5', row: 'a', column: 5, status: 'AVAILABLE' },
+        a1: { id: 'a1', row: 'a', column: 1, status: 'AVAILABLE' },
+        a6: { id: 'a6', row: 'a', column: 6, status: 'AVAILABLE' },
+        a7: { id: 'a7', row: 'a', column: 7, status: 'AVAILABLE' },
+        a8: { id: 'a8', row: 'a', column: 8, status: 'AVAILABLE' },
+        a9: { id: 'a9', row: 'a', column: 9, status: 'AVAILABLE' },
+        a10: { id: 'a10', row: 'a', column: 10, status: 'AVAILABLE' },
+      })
+      reserved_seats = theatre.reserve_seats(4)
+      expect(reserved_seats.map(&:id)).to eq(['a3', 'a4', 'a5', 'a6'])
     end
   end
 end
